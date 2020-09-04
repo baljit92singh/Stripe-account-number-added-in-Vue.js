cfbc39a25e2b25b86b962a5753530e9c82bd9b21
Baljit Singh <baljit@bitcot.com>
2020-09-04 14:30:21 +0530

    added strip section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,123 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form @submit.prevent="sendDetail">
+      <input type="hidden" name="token" />
+      <div class="form-row">
+        <label for="country">Country</label>
+        <select id="country">
+          <option value="US">United States</option>
+          <option value="CA">Canada</option>
+          <option value="GB">United Kingdom</option>
+          <option value="DE">Germany</option>
+          <option value="FR">France</option>
+          <option value="ES">Spain</option>
+        </select>
+      </div>
+      <div class="form-row">
+        <label for="currency">Currency</label>
+        <select id="currency">
+          <option value="USD">US Dollar</option>
+          <option value="CAD">Canadian Dollar</option>
+          <option value="GBP">Pound</option>
+          <option value="EUR">Euro</option>
+        </select>
+      </div>
+      <div class="form-row">
+        <label for="routing-number">Routing Number</label>
+        <input type="text" id="routing-number" value="110000000" />
+      </div>
+      <div class="form-row">
+        <label for="account-number">Account Number</label>
+        <input type="text" id="account-number" value="000123456789" />
+      </div>
+      <div class="form-row">
+        <label for="account-holder-name">Account Holder Name</label>
+        <input type="text" id="account-holder-name" value="Jane Austen" />
+      </div>
+      <div class="form-row">
+        <label for="account-holder-type">Account Holder Type</label>
+        <select id="account-holder-type">
+          <option value="individual">Individual</option>
+          <option value="company">Company</option>
+        </select>
+      </div>
+      <button type="submit">Submit</button>
+      <div class="outcome">
+        <div class="error"></div>
+        <div class="success">
+          Success! Your Stripe token is
+          <span class="token"></span>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      stripe: window.Stripe("ENTER YOUR PUBLIC STRIP KEY"),
+      elements: "",
+    };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    setOutcome(result) {
+      var successElement = document.querySelector(".success");
+      var errorElement = document.querySelector(".error");
+      successElement.classList.remove("visible");
+      errorElement.classList.remove("visible");
+      if (result.token) {
+        // In this example, we're simply displaying the token
+        successElement.querySelector(".token").textContent = result.token.id;
+        successElement.classList.add("visible");
+
+        // In a real integration, you'd submit the form with the token to your backend server
+        //var form = document.querySelector('form');
+        //form.querySelector('input[name="token"]').setAttribute('value', result.token.id);
+        //form.submit();
+      } else {
+        errorElement.textContent = result.error.message;
+        errorElement.classList.add("visible");
+      }
+    },
+    sendDetail() {
+      var bankAccountParams = {
+        country: document.getElementById("country").value,
+        currency: document.getElementById("currency").value,
+        account_number: document.getElementById("account-number").value,
+        account_holder_name: document.getElementById("account-holder-name")
+          .value,
+        account_holder_type: document.getElementById("account-holder-type")
+          .value,
+      };
+      if (document.getElementById("routing-number").value != "") {
+        bankAccountParams["routing_number"] = document.getElementById(
+          "routing-number"
+        ).value;
+      }
+      this.stripe
+        .createToken("bank_account", bankAccountParams)
+        .then(this.setOutcome);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.success,
+.error {
+  display: none;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.success.visible,
+.error.visible {
+  display: inline;
 }
 </style>
